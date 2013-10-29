62a322ba0e233821cade855430b9ad974708d64b
xmxurey <mengxingxu@UREY.local>
2013-10-29 16:51:25 +0800

    customer

---
+++ BookingHistory.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mengxingxu
+ */
+public class BookingHistory {
+    private Payment[] payment;
+    
+    public BookingHistory(){
+    }
+    
+    public BookingHistory(Payment[] payment, Customer customer){
+    }
+    
+    public void PrintBookingHistory(){
+        
+    }
+}
---
+++ Payment.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mengxingxu
+ */
+public class Payment {
+    private Ticket[] tickets;
+    private double totalprice;
+    private Date purchaseDate;
+    
+    public void getInVoice(){
+        
+    }
+    
+    public void calculateTotalPrice(){
+        
+    }
+    
+    public double getTotalPrice(){
+        return totalprice;
+    }
+    
+}
